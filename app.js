903ae84fcf3ec273a14cfbda11ac8f06b7735e5c
LilDevsy0117 <skkulsy0117@naver.com>
2024-03-28 01:05:45 +0900

    peace

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const http = require('http');
 // create server
 // requestListner is excute for every incomming request
 // first is requset obj second is respone obj
-
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 //keep ruunig to listen
