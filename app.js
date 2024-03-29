79993ada6b05ee6f8be065948246d8dc48b1580d
LilDevsy0117 <skkulsy0117@naver.com>
2024-03-29 17:51:26 +0900

    use

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require('express');
 
 const app = express();
 
+// req, res, next(fucntion by express js)
+app.use((req, res, next) => { 
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware in line
+}); 
+
+app.use((req, res, next) => { 
+    console.log('In another middleware');
+    // ...
+}); 
+
 const server = http.createServer(app);
 
 server.listen(3000);
